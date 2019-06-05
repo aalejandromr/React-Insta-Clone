860d5ac642c562d8c41cf856d877fe70b5d0fb81
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-06-05 15:42:57 -0400

    Refactor code to work with withAuthenticate HOC

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,99 +1,16 @@
 import React from 'react';
 import './App.css';
-import dummyData from './dummy-data.js';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faComment, faHeart, faCompass, faUser
-} from '@fortawesome/free-regular-svg-icons'
-import PostContainer from './components/postContainer/PostContainer';
-import CommentForm from './components/CommentForm/CommentForm';
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
-import SearchBar from './components/searchBar/SearchBar'
 
-library.add(faComment, faHeart, faInstagram, faCompass, faUser, faSearch);
+import withAthenticate from './components/authentication/Authentication';
+import { PostPage } from './components/postContainer/PostPage';
+
+const ComponentFromWithAuthenticate = withAthenticate(PostPage);
 
 class App extends React.Component {
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      posts: dummyData,
-      filtered: dummyData,
-      search: ""
-    }
-  }
-
-  componentDidMount()
-  {
-    if(localStorage.length > 0) {
-      this.setState({
-        filtered: JSON.parse(localStorage.posts),
-        posts: JSON.parse(localStorage.posts)
-      })
-    }
-    window.addEventListener('beforeunload', () => {
-      for (let key in this.state) {
-        // save to localStorage
-        localStorage.setItem(key, JSON.stringify(this.state[key]));
-      }
-    })
-  }
-
-  handleSubmitComment = (e, newComment, postKey) => {
-    this.setState(prevState => {
-      return {
-        posts: prevState.posts.map((post, key) => {
-          if(key === postKey) {
-            post.comments.push({username: "aalejandromr", text: newComment})
-          }
-          return post;
-        })
-      }
-    })
-    e.preventDefault();
-  }
-
-  handleLikes = (postKey) => {
-    this.setState(prevState => {
-      return {
-        posts: prevState.posts.map((post, key) => {
-          if(key === postKey) {
-            post.likes++
-          }
-          return post;
-        })
-      }
-    })
-  }
-
-  handleSearch = (e) => {
-    this.setState({
-      search: e.target.value
-    })
-  }
 
   render (){
     return (
-      <div className="App">
-        <header>
-          <SearchBar handleSearch={this.handleSearch} search={this.state.search}/>
-        </header>
-        <section className="main-body">
-          {
-            this.state.posts.map((post, key) => {
-              if(RegExp(this.state.search,"i").test(post.username)){
-                return (
-                  <section className="post-wrapper" key={key}>
-                      <PostContainer post={post} handleLikes={() => this.handleLikes(key)}/>
-                      <CommentForm handleAddition={this.handleSubmitComment} postKey={key}/>
-                  </section>
-                  )
-              }
-            })
-          }
-        </section>
-      </div>
+      <ComponentFromWithAuthenticate/>
     );
   }
 }
---
+++ instagram-app/src/components/postContainer/PostPage.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import dummyData from '../../dummy-data';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faComment, faHeart, faCompass, faUser
+} from '@fortawesome/free-regular-svg-icons'
+import PostContainer from '../postContainer/PostContainer';
+import CommentForm from '../CommentForm/CommentForm'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import SearchBar from '../searchBar/SearchBar'
+
+library.add(faComment, faHeart, faInstagram, faCompass, faUser, faSearch);
+
+export class PostPage extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      posts: dummyData,
+      filtered: dummyData,
+      search: ""
+    }
+  }
+
+  componentDidMount()
+  {
+    if(localStorage.length > 0) {
+      this.setState({
+        filtered: JSON.parse(localStorage.posts),
+        posts: JSON.parse(localStorage.posts)
+      })
+    }
+    window.addEventListener('beforeunload', () => {
+      for (let key in this.state) {
+        // save to localStorage
+        localStorage.setItem(key, JSON.stringify(this.state[key]));
+      }
+    })
+  }
+
+  handleSubmitComment = (e, newComment, postKey) => {
+    this.setState(prevState => {
+      return {
+        posts: prevState.posts.map((post, key) => {
+          if(key === postKey) {
+            post.comments.push({username: "aalejandromr", text: newComment})
+          }
+          return post;
+        })
+      }
+    })
+    e.preventDefault();
+  }
+
+  handleLikes = (postKey) => {
+    this.setState(prevState => {
+      return {
+        posts: prevState.posts.map((post, key) => {
+          if(key === postKey) {
+            post.likes++
+          }
+          return post;
+        })
+      }
+    })
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <header>
+          <SearchBar handleSearch={this.handleSearch} search={this.state.search}/>
+        </header>
+        <section className="main-body">
+          {
+            this.state.posts.map((post, key) => {
+              if(RegExp(this.state.search,"i").test(post.username)){
+                return (
+                  <section className="post-wrapper" key={key}>
+                      <PostContainer post={post} handleLikes={() => this.handleLikes(key)}/>
+                      <CommentForm handleAddition={this.handleSubmitComment} postKey={key}/>
+                  </section>
+                  )
+              }
+            })
+          }
+        </section>
+      </div>
+    );
+  }
+}
